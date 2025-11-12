575a724ad2b6ac36607759aa3d4a7ed8be8428aa
ifoca <foca.ionut@gmail.com>
2025-11-12 14:18:49 +0100

    add function to create cards

--- src/main.js
+++ src/main.js
@@ -1,25 +1,30 @@
+const cardsContainer = document.getElementById('cards-container');
+const cardStyleClass = 'bg-gray-300 p-2 mt-2 rounded-lg';
+
 let allPokemon = [];
 let url = 'https://pokeapi.co/api/v2/pokemon?limit=20';
 
 const path = 'https://pokeapi.co/api/v2/pokemon?limit=100';
 // const next = 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=100'
 
-// gets all 1328 pokemon
+/* 
+ To gets all 1328 pokemon
 
-// async function fetchAllPokemon() {
-//   while (url) {
-//     const res = await fetch(url);
-//     const data = await res.json();
+    async function fetchAllPokemon() {
+    while (url) {
+        const res = await fetch(url);
+        const data = await res.json();
 
-//     allPokemon = allPokemon.concat(data.results);
-//     url = data.next;
-//     console.log('Still fetching...');
-//   }
-//   console.log(`Fetched ${allPokemon.length} Pokemon`);
-//   return allPokemon;
-// }
+        allPokemon = allPokemon.concat(data.results);
+        url = data.next;
+        console.log('Still fetching...');
+    }
+    console.log(`Fetched ${allPokemon.length} Pokemon`);
+    return allPokemon;
+    }
+    fetchAllPokemon();
 
-// fetchAllPokemon();
+*/
 
 // get only x amount of pokemon
 fetch(path)
@@ -28,9 +33,23 @@ fetch(path)
     return res.json();
   })
   .then((data) => {
-    console.log('Raw data:', data.results);
+    const results = data.results;
+    console.log('Raw data:', results);
+    results.forEach((result) => {
+      createCard(result);
+    });
   })
   .catch(console.error);
+
+// create function that takes the pokemon fetch response as an argument
+// use the data from the response to populate the card
+// fill in the card with the required information
+function createCard(pokemon) {
+  const cardDiv = document.createElement('div');
+  cardDiv.textContent = pokemon.name;
+  cardDiv.className = cardStyleClass;
+  cardsContainer.appendChild(cardDiv);
+}
 
 // GET pokemon by name endpoint
 // endpoint https://pokeapi.co/api/v2/pokemon/ivysaur
