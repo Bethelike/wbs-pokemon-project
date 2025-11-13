dec6b86b9d013769eb58038061287467d72b75e3
ifoca <foca.ionut@gmail.com>
2025-11-13 17:53:01 +0100

    add elements to the card

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,6 @@ let allPokemon = [];
 let url = 'https://pokeapi.co/api/v2/pokemon?limit=20';
 
 const path = 'https://pokeapi.co/api/v2/pokemon?limit=100';
-// const next = 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=100'
 
 async function fetchAllPokemon(path) {
   try {
@@ -40,7 +39,8 @@ async function loadPokemon(path) {
 fetchAllPokemon(url).then((results) => {
   console.log(results[0]);
   results.forEach((result) => {
-    loadPokemon(result.url).then((pokemon) => {
+    const pokemonIdPath = result.url;
+    loadPokemon(pokemonIdPath).then((pokemon) => {
       console.log(pokemon);
       createCard(pokemon);
     });
@@ -54,12 +54,22 @@ function createCard(pokemon) {
   const cardDiv = document.createElement('div');
   const cardTitle = document.createElement('h6');
   const image = document.createElement('img');
-  cardDiv.textContent = pokemon.name;
-  cardDiv.className = cardStyleClass;
+  const span = document.createElement('span');
+  const catchBtn = document.createElement('button');
+
+  cardDiv.id = pokemon.id;
+  cardDiv.classList = `${pokemon.name} flex flex-col items-center ${cardStyleClass}`;
+  cardTitle.textContent = pokemon.name;
+  image.src = pokemon.sprites.other['official-artwork'].front_default;
+  span.textContent = `Stats: ${pokemon.stats[0].base_stat} ...`;
+  catchBtn.textContent = 'Catch it!';
+  catchBtn.classList = 'mt-2 p-3 border-3 border-gray-100';
 
   cardsContainer.appendChild(cardDiv);
   cardDiv.appendChild(cardTitle);
   cardDiv.appendChild(image);
+  cardDiv.appendChild(span);
+  cardDiv.appendChild(catchBtn);
 }
 
 // GET pokemon by name endpoint
@@ -67,59 +77,3 @@ function createCard(pokemon) {
 
 // GET pokemon by id endpoint
 // https://pokeapi.co/api/v2/pokemon/1
-
-/*
-
-const cardsContainer = document.getElementById('pokemon-container');
-const cardStyleClass = 'bg-gray-300 p-4 rounded-lg capitalize';
-
-let allPokemon = [];
-// let url = 'https://pokeapi.co/api/v2/pokemon?limit=20';
-
-const path = 'https://pokeapi.co/api/v2/pokemon?limit=150';
-// const next = 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=100'
-
-const pokemonByNamePath = '';
-const pokemonByIdPath = 'https://pokeapi.co/api/v2/pokemon/1/';
-
-// get only x amount of pokemon
-fetch(path)
-  .then((res) => {
-    if (!res.ok) throw new Error('Something went wrong');
-    return res.json();
-  })
-  .then((data) => {
-    const results = data.results;
-    results.forEach((result) => {
-      loadPokemon(result.url).then((pokemon) => {
-        console.log(pokemon);
-        createCard(pokemon);
-      });
-    });
-  })
-  .catch(console.error);
-
-// create function that takes the pokemon fetch response as an argument
-// use the data from the response to populate the card
-// fill in the card with the required information
-function createCard(pokemon) {
-  const cardDiv = document.createElement('div');
-  const cardHeader = document.createElement('h6');
-  const img = document.createElement('img');
-  const span = document.createElement('span');
-
-  cardHeader.textContent = pokemon.name;
-  cardDiv.className = cardStyleClass;
-  img.src = pokemon.sprites.other['official-artwork'].front_default;
-  span.textContent = `Type: ${pokemon.types[0].type.name}`;
-  // console.log(span.textContent);
-  cardDiv.appendChild(cardHeader);
-  cardDiv.appendChild(img);
-  cardDiv.appendChild(span);
-  cardsContainer.appendChild(cardDiv);
-}
-
-
-
-
-*/
